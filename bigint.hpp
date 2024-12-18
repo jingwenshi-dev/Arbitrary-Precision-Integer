53a4caf73568b30db1b97728b2937561aa74b222
Jingwen Shi <jingwensteven.shi@gmail.com>
2024-12-17 19:21:18 -0500

    Add docstring

--- bigint.hpp
+++ bigint.hpp
@@ -13,14 +13,22 @@ private:
     bool isNegative{};
     std::vector<std::uint8_t> digits;
 
+    /**
+     * @brief Remove leading zeros during calculations or when allocating space for the vector
+     * @param digits Vector of digits to be processed
+     */
     static void remove_leading_zeros(std::vector<std::uint8_t> &digits) {
         while (digits.size() > 1 && digits.back() == 0) {
             digits.pop_back();
         }
     }
 
+    /**
+     * @brief Turn the string representation into the vector of digit and handle the sign.
+     * @param str String to be converted into bigint
+     */
     void str_to_bigint(const std::string &str) {
-        isNegative = str[0] == '-';
+        this->isNegative = str[0] == '-';
         size_t start = 0;
 
         if (str[0] == '-' || str[0] == '+') {
@@ -38,9 +46,15 @@ private:
 
         // Handle negative zero
         remove_leading_zeros(this->digits);
-        if (is_abs_zero(this->digits)) isNegative = false;
+        if (is_abs_zero(this->digits)) this->isNegative = false;
     }
 
+    /**
+     * @brief Check if the absolute value of lhs is less than the absolute value of rhs
+     * @param lhs Vector of digits of the first number
+     * @param rhs Vector of digits of the second number
+     * @return True iff lhs is less than rhs
+     */
     static bool is_abs_less_than(const std::vector<std::uint8_t> &lhs, const std::vector<std::uint8_t> &rhs) {
         if (lhs.size() != rhs.size()) return lhs.size() < rhs.size();
 
@@ -51,21 +65,32 @@ private:
         return false;
     }
 
+    /**
+     * @brief Check if the absolute value of num is zero. Consecutive zeros are not considered zero.
+     * @param num Vector of digits to be checked
+     * @return True iff the number is zero
+     */
     static bool is_abs_zero(const std::vector<std::uint8_t> &num) {
         return num.size() == 1 && num[0] == 0;
     }
 
+    /**
+     * @brief Add the absolute values of two numbers
+     * @param big Vector of digits of the larger number
+     * @param small Vector of digits of the second number
+     * @return A vector of digits smaller the sum of the two numbers' absolute values
+     */
     static std::vector<std::uint8_t>
-    add_abs(const std::vector<std::uint8_t> &lhs, const std::vector<std::uint8_t> &rhs) {
+    add_abs(const std::vector<std::uint8_t> &big, const std::vector<std::uint8_t> &small) {
         std::vector<std::uint8_t> result;
-        result.resize(std::max(lhs.size(), rhs.size()));
+        result.resize(std::max(big.size(), small.size()));
         result.reserve(result.size() + 1); // Max size, e.g. 999 + 999 = 1998 -> 4 digits
 
         std::uint8_t carry = 0;
 
-        for (std::size_t i = 0; i < lhs.size(); i++) {
-            std::uint8_t sum = carry + lhs[i];
-            if (i < rhs.size()) sum += rhs[i];
+        for (std::size_t i = 0; i < big.size(); i++) {
+            std::uint8_t sum = carry + big[i];
+            if (i < small.size()) sum += small[i];
 
             result[i] = sum % 10;
             carry = sum / 10;
@@ -75,6 +100,12 @@ private:
         return result;
     }
 
+    /**
+     * @brief Subtract the absolute values of two numbers
+     * @param big Vector of digits of the larger number
+     * @param small Vector of digits of the smaller number
+     * @return A vector of digits representing the difference of the two numbers' absolute values
+     */
     static std::vector<std::uint8_t> subtract_abs(const std::vector<std::uint8_t> &big,
                                                   const std::vector<std::uint8_t> &small) {
         std::vector<std::uint8_t> result;
@@ -101,6 +132,12 @@ private:
         return result;
     }
 
+    /**
+     * @brief Multiply the absolute values of two numbers
+     * @param lhs Vector of digits of the first number
+     * @param rhs Vector of digits of the second number
+     * @return A vector of digits representing the product of the two numbers' absolute values
+     */
     static std::vector<std::uint8_t> multiply_abs(const std::vector<std::uint8_t> &lhs,
                                                   const std::vector<std::uint8_t> &rhs) {
         if (is_abs_zero(lhs) || is_abs_zero(rhs)) return {0};
@@ -129,12 +166,18 @@ private:
     }
 
 public:
-    // Constructors
+    /**
+     * @brief Default constructor: Initialize the number to zero
+     */
     explicit bigint() {
         isNegative = false;
         digits.push_back(0);
     }
 
+    /**
+     * @brief Int64 constructor: Initialize the number to the given integer
+     * @param num Integer to be converted to bigint
+     */
     explicit bigint(std::int64_t num) {
         isNegative = num < 0;
 
@@ -156,14 +199,19 @@ public:
         }
     }
 
+    /**
+     * @brief String constructor: Initialize the number to the given string
+     * @param str String to be converted to bigint
+     */
     explicit bigint(const std::string &str) {
         if (str.empty()) throw std::invalid_argument("Empty string is not permitted");
         str_to_bigint(str);
     }
 
-    // Operators
-
-    // Negation
+    /**
+     * @brief Negation operator: transform the current number to its negation
+     * @return The negation of the current number
+     */
     bigint operator-() const {
         // Reference: https://www.geeksforgeeks.org/shallow-copy-and-deep-copy-in-c/
         bigint negative = *this;
@@ -176,33 +224,49 @@ public:
         return negative;
     }
 
-    // Increment: Prefix
+    /**
+     * @brief Increment operator (prefix): Add 1 to the current number
+     * @return The incremented number
+     */
     bigint &operator++() {
         *this += bigint(1);
         return *this;
     }
 
-    // Increment: Postfix
+    /**
+     * @brief Increment operator (postfix): Add 1 to the current number
+     * @return The original number before increment
+     */
     bigint operator++(int) {
         bigint temp = *this;
         *this += bigint(1);
         return temp;
     }
 
-    // Decrement: Prefix
+    /**
+     * @brief Decrement operator (prefix): Subtract 1 from the current number
+     * @return The decremented number
+     */
     bigint &operator--() {
         *this -= bigint(1);
         return *this;
     }
 
-    // Decrement: Postfix
+    /**
+     * @brief Decrement operator (postfix): Subtract 1 from the current number
+     * @return The original number before decrement
+     */
     bigint operator--(int) {
         bigint temp = *this;
         *this -= bigint(1);
         return temp;
     }
 
-    // Compound Assignment Operators
+    /**
+     * @brief Addition Assignment Operator: Add <rhs> to the current number
+     * @param rhs Number to be added to the current number
+     * @return The current number after addition
+     */
     bigint &operator+=(const bigint &rhs) {
         // Add abs values if signs are the same
         if (this->isNegative == rhs.isNegative) {
@@ -230,11 +294,21 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Subtraction Assignment Operator: Subtract another number from the current number
+     * @param rhs Number to be subtracted from the current number
+     * @return The current number after subtraction
+     */
     bigint &operator-=(const bigint &rhs) {
         *this += -rhs;
         return *this;
     }
 
+    /**
+     * @brief Multiplication Assignment Operator: Multiply another number to the current number
+     * @param rhs Number to be multiplied to the current number
+     * @return The current number after multiplication
+     */
     bigint &operator*=(const bigint &rhs) {
         this->isNegative = this->isNegative != rhs.isNegative;
         this->digits = multiply_abs(this->digits, rhs.digits);
@@ -243,31 +317,62 @@ public:
         return *this;
     }
 
-    // Binary Operators
     // Friend functions to allow using compound assignment operators as it will call private parameters.
+    /**
+     * @brief Addition Operator: Add two numbers
+     * @param lhs The first number to be added
+     * @param rhs The second number to be added
+     * @return A new number that is the sum of the two numbers
+     */
     friend bigint operator+(bigint lhs, const bigint &rhs) {
         return lhs += rhs;
     }
 
+    /**
+     * @brief Subtraction Operator: Subtract two numbers
+     * @param lhs The number to be subtracted from
+     * @param rhs THe number to be subtracted
+     * @return A new number that is the difference of the two numbers
+     */
     friend bigint operator-(bigint lhs, const bigint &rhs) {
         return lhs -= rhs;
     }
 
+    /**
+     * @brief Multiplication Operator: Multiply two numbers
+     * @param lhs The first number to be multiplied
+     * @param rhs The second number to be multiplied
+     * @return A new number that is the product of the two numbers
+     */
     friend bigint operator*(bigint lhs, const bigint &rhs) {
         return lhs *= rhs;
     }
 
-    // Comparison Operators
+    /**
+     * @brief Equality Operator: Check if two numbers are equal
+     * @param rhs The number to be compared with
+     * @return True iff the two numbers are equal
+     */
     bool operator==(const bigint &rhs) const {
         // Reference: https://learn.microsoft.com/en-us/cpp/cpp/equality-operators-equal-equal-and-exclpt-equal?view=msvc-170
         // In c++, == compares the value of obj, not the address
         return this->isNegative == rhs.isNegative && this->digits == rhs.digits;
     }
 
+    /**
+     * @brief Inequality Operator: Check if two numbers are not equal
+     * @param rhs The number to be compared with
+     * @return True iff the two numbers are not equal
+     */
     bool operator!=(const bigint &rhs) const {
         return !(*this == rhs);
     }
 
+    /**
+     * @brief Less Than Operator: Check if the current number is less than another number
+     * @param rhs The number to be compared with
+     * @return True iff the current number is less than <rhs>
+     */
     bool operator<(const bigint &rhs) const {
         if (this->isNegative && !rhs.isNegative) {
             return true;
@@ -280,22 +385,42 @@ public:
         return is_abs_less_than(this->digits, rhs.digits);
     }
 
+    /**
+     * @brief Less Than or Equal Operator: Check if the current number is less than or equal to another number
+     * @param rhs The number to be compared with
+     * @return True iff the current number is less than or equal to <rhs>
+     */
     bool operator<=(const bigint &rhs) const {
         if (*this == rhs) return true;
         return *this < rhs;
     }
 
+    /**
+     * @brief Greater Than Operator: Check if the current number is greater than another number
+     * @param rhs The number to be compared with
+     * @return True iff the current number is greater than <rhs>
+     */
     bool operator>(const bigint &rhs) const {
         return !(*this <= rhs);
     }
 
+    /**
+     * @brief Greater Than or Equal Operator: Check if the current number is greater than or equal to another number
+     * @param rhs The number to be compared with
+     * @return True iff the current number is greater than or equal to <rhs>
+     */
     bool operator>=(const bigint &rhs) const {
         return !(*this < rhs);
     }
 
-    // Insertion
-    // Reference: https://stackoverflow.com/questions/476272/how-can-i-properly-overload-the-operator-for-an-ostream
+    /**
+     * @brief Insertion Operator: Send the number to the output stream
+     * @param stream Output stream to send the number
+     * @param num Number to be sent
+     * @return The output stream after sending the number
+     */
     friend std::ostream &operator<<(std::ostream &stream, const bigint &num) {
+        // Reference: https://stackoverflow.com/questions/476272/how-can-i-properly-overload-the-operator-for-an-ostream
         if (num.isNegative) stream << '-';
 
         // Reference: https://stackoverflow.com/questions/3610933/iterating-c-vector-from-the-end-to-the-beginning
